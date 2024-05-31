f00fc8fe98dd1d3c0e3108dd265e1453ed3e3429
IfeoluwaOyekanmi <ifeoluwagodishere@gmail.com>
2024-05-31 11:45:24 +0100

    adding-api-key

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-
+import React, {useState} from 'react';
+import axios from 'axios';
 
 function App() {
-  return (
+
+const url = `https://api.openweathermap.org/data/2.5/weather?q=osogbo&appid=4d4323d78908e08ac921e0c09e0d2060`  return (
     <div className="App">
       
     </div>
