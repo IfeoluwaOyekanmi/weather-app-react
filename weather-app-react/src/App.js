dcaa67b1000bedd51dc0ef153e434d496632dce5
IfeoluwaOyekanmi <ifeoluwagodishere@gmail.com>
2024-05-31 12:52:05 +0100

    top-search-bar-completed

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -2,10 +2,31 @@ import React, {useState} from 'react';
 import axios from 'axios';
 
 function App() {
+  const [data,setData] = useState ({})
+  const [location,setLocation] =useState ('')
 
 const url = `https://api.openweathermap.org/data/2.5/weather?q=osogbo&appid=4d4323d78908e08ac921e0c09e0d2060`  
+
+const searchLocation = (event) => {
+  if (event.key === 'Enter')
+  axios.get(url).then((response) => {
+    setData(response.data)
+    console.log(response.data)
+  }) 
+}   
+ 
+
 return (
     <div className="app">
+        <div className="search">
+        <input
+        value={location}
+        onChange={event => setLocation(event.target.value)}
+        onKeyPress={searchLocation}
+        placeholder='Enter Location'
+        type='text' />
+        </div>
+
       <div className="container">
         <div className="top">
           <div className="location">
