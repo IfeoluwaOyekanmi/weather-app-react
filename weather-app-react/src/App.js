c8b90e0831e9718a29c1ffeb2021518f59b8af7c
IfeoluwaOyekanmi <ifeoluwagodishere@gmail.com>
2024-05-31 11:58:15 +0100

    HTML-structure-of-landing-page-completed

--- weather-app-react/src/App.js
+++ weather-app-react/src/App.js
@@ -3,9 +3,36 @@ import axios from 'axios';
 
 function App() {
 
-const url = `https://api.openweathermap.org/data/2.5/weather?q=osogbo&appid=4d4323d78908e08ac921e0c09e0d2060`  return (
-    <div className="App">
-      
+const url = `https://api.openweathermap.org/data/2.5/weather?q=osogbo&appid=4d4323d78908e08ac921e0c09e0d2060`  
+return (
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Osogbo</p>
+          </div>
+          <div className="temp">
+            <h1>65°F</h1>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p className='bold'>65°F</p>
+            <p >Feels Like</p>
+          </div>
+          <div className="humidity">
+            <p className='bold'>20%</p>
+            <p>Humidity</p> 
+          </div>
+          <div className="wind">
+            <p className='bold'>12 MPH</p>
+            <p>Wind Speed</p>
+          </div>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
